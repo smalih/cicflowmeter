88b97e61870b08991eae874c4ac23aa9606c1112
Solomon Malih <smalih15@hotmail.com>
2025-02-04 11:28:22 +0000

    Updated flow.py data columns to match that of CICIDS-2017 dataset - fixed minor consistencies with regards to ordering of columns in dataset

--- src/cicflowmeter/flow.py
+++ src/cicflowmeter/flow.py
@@ -89,19 +89,15 @@ class Flow:
 
         data = {
             # Basic IP information
-            "src_ip": self.src_ip,
-            "dst_ip": self.dest_ip,
-            "src_port": self.src_port,
+            # "src_ip": self.src_ip,
+            # "dst_ip": self.dest_ip,
+            # "src_port": self.src_port,
             "dst_port": self.dest_port,
-            "protocol": self.protocol,
+            # "protocol": self.protocol,
             # Basic information from packet times
-            "timestamp": packet_time.get_timestamp(),
+            # "timestamp": packet_time.get_timestamp(),
             "flow_duration": packet_time.get_duration(),
-            "flow_byts_s": flow_bytes.get_rate(),
-            "flow_pkts_s": packet_count.get_rate(),
-            "fwd_pkts_s": packet_count.get_rate(PacketDirection.FORWARD),
-            "bwd_pkts_s": packet_count.get_rate(PacketDirection.REVERSE),
-            # Count total packets by direction
+             # Count total packets by direction
             "tot_fwd_pkts": packet_count.get_total(PacketDirection.FORWARD),
             "tot_bwd_pkts": packet_count.get_total(PacketDirection.REVERSE),
             # Statistical info obtained from Packet lengths
@@ -115,61 +111,89 @@ class Flow:
             "bwd_pkt_len_min": packet_length.get_min(PacketDirection.REVERSE),
             "bwd_pkt_len_mean": packet_length.get_mean(PacketDirection.REVERSE),
             "bwd_pkt_len_std": packet_length.get_std(PacketDirection.REVERSE),
-            "pkt_len_max": packet_length.get_max(),
-            "pkt_len_min": packet_length.get_min(),
-            "pkt_len_mean": packet_length.get_mean(),
-            "pkt_len_std": packet_length.get_std(),
-            "pkt_len_var": packet_length.get_var(),
-            "fwd_header_len": flow_bytes.get_forward_header_bytes(),
-            "bwd_header_len": flow_bytes.get_reverse_header_bytes(),
-            "fwd_seg_size_min": flow_bytes.get_min_forward_header_bytes(),
-            "fwd_act_data_pkts": packet_count.has_payload(PacketDirection.FORWARD),
+
+            "flow_byts_s": flow_bytes.get_rate(),
+            "flow_pkts_s": packet_count.get_rate(),
+            
             # Flows Interarrival Time
             "flow_iat_mean": flow_iat["mean"],
+            "flow_iat_std": flow_iat["std"],
             "flow_iat_max": flow_iat["max"],
             "flow_iat_min": flow_iat["min"],
-            "flow_iat_std": flow_iat["std"],
+            
             "fwd_iat_tot": forward_iat["total"],
+            "fwd_iat_mean": forward_iat["mean"],
+             "fwd_iat_std": forward_iat["std"],
             "fwd_iat_max": forward_iat["max"],
             "fwd_iat_min": forward_iat["min"],
-            "fwd_iat_mean": forward_iat["mean"],
-            "fwd_iat_std": forward_iat["std"],
+
             "bwd_iat_tot": backward_iat["total"],
-            "bwd_iat_max": backward_iat["max"],
-            "bwd_iat_min": backward_iat["min"],
             "bwd_iat_mean": backward_iat["mean"],
             "bwd_iat_std": backward_iat["std"],
+            "bwd_iat_max": backward_iat["max"],
+            "bwd_iat_min": backward_iat["min"],
+           
+
             # Flags statistics
             "fwd_psh_flags": flag_count.count("PSH", PacketDirection.FORWARD),
             "bwd_psh_flags": flag_count.count("PSH", PacketDirection.REVERSE),
             "fwd_urg_flags": flag_count.count("URG", PacketDirection.FORWARD),
             "bwd_urg_flags": flag_count.count("URG", PacketDirection.REVERSE),
+            "fwd_header_len": flow_bytes.get_forward_header_bytes(),
+            "bwd_header_len": flow_bytes.get_reverse_header_bytes(),
+
+            "fwd_pkts_s": packet_count.get_rate(PacketDirection.FORWARD),
+            "bwd_pkts_s": packet_count.get_rate(PacketDirection.REVERSE),
+
+            "pkt_len_min": packet_length.get_min(),
+            "pkt_len_max": packet_length.get_max(),
+            
+            "pkt_len_mean": packet_length.get_mean(),
+            "pkt_len_std": packet_length.get_std(),
+            "pkt_len_var": packet_length.get_var(),         
+            
             "fin_flag_cnt": flag_count.count("FIN"),
             "syn_flag_cnt": flag_count.count("SYN"),
             "rst_flag_cnt": flag_count.count("RST"),
             "psh_flag_cnt": flag_count.count("PSH"),
             "ack_flag_cnt": flag_count.count("ACK"),
             "urg_flag_cnt": flag_count.count("URG"),
+            "cwr_flag_count": None, # duplicate feature - CICIDS dataset labels this 'CWE Flag Count' - assuming to be a typo
             "ece_flag_cnt": flag_count.count("ECE"),
+            
             # Response Time
             "down_up_ratio": packet_count.get_down_up_ratio(),
             "pkt_size_avg": packet_length.get_avg(),
-            "init_fwd_win_byts": self.init_window_size[PacketDirection.FORWARD],
-            "init_bwd_win_byts": self.init_window_size[PacketDirection.REVERSE],
-            "active_max": active_stat["max"],
-            "active_min": active_stat["min"],
-            "active_mean": active_stat["mean"],
-            "active_std": active_stat["std"],
-            "idle_max": idle_stat["max"],
-            "idle_min": idle_stat["min"],
-            "idle_mean": idle_stat["mean"],
-            "idle_std": idle_stat["std"],
+            "fwd_seg_size_avg": None, # duplicate feature
+            "bwd_seg_size_avg": None, # duplicate feature
+            # fwd_header_len: None, literally included twice in the dataset
             "fwd_byts_b_avg": flow_bytes.get_bytes_per_bulk(PacketDirection.FORWARD),
             "fwd_pkts_b_avg": flow_bytes.get_packets_per_bulk(PacketDirection.FORWARD),
+            "fwd_blk_rate_avg": flow_bytes.get_bulk_rate(PacketDirection.FORWARD),
             "bwd_byts_b_avg": flow_bytes.get_bytes_per_bulk(PacketDirection.REVERSE),
             "bwd_pkts_b_avg": flow_bytes.get_packets_per_bulk(PacketDirection.REVERSE),
-            "fwd_blk_rate_avg": flow_bytes.get_bulk_rate(PacketDirection.FORWARD),
             "bwd_blk_rate_avg": flow_bytes.get_bulk_rate(PacketDirection.REVERSE),
+
+            "subflow_fwd_pkts": None, # duplicate feature
+            "subflow_fwd_byts": None, # duplicate feature
+            "subflow_bwd_pkts": None, # duplicate feature
+            "subflow_bwd_byts": None, # duplicate feature
+
+            "init_fwd_win_byts": self.init_window_size[PacketDirection.FORWARD],
+            "init_bwd_win_byts": self.init_window_size[PacketDirection.REVERSE],
+            "fwd_act_data_pkts": packet_count.has_payload(PacketDirection.FORWARD),
+            "fwd_seg_size_min": flow_bytes.get_min_forward_header_bytes(),
+            
+            "active_mean": active_stat["mean"],
+            "active_std": active_stat["std"],
+            "active_max": active_stat["max"],
+            "active_min": active_stat["min"],
+            
+            "idle_mean": idle_stat["mean"],
+            "idle_std": idle_stat["std"],
+            "idle_max": idle_stat["max"],
+            "idle_min": idle_stat["min"],           
+            
         }
 
         # Duplicated features
